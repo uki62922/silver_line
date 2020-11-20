43e61d0fcf05a6892990e9c96108876ba8a9e494
uki62922 <uki62922@yahoo.co.jp>
2020-11-20 19:00:16 +0900

    リンクの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
-  resources :users, only: [:edit, :update,:show]
+  resources :users, only: [:edit, :update,:show]do
+    collection do
+      get 'search'
+    end
+  end
   resources :rooms, only: [:new, :create]
 
 end
