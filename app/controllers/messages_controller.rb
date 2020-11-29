0dd5670a3c81cecb9cd495f6707aa6a62c7d3fb0
uki62922 <uki62922@yahoo.co.jp>
2020-11-29 18:33:46 +0900

    バリデーション追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,23 +1,32 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new
     @room = Room.find(params[:room_id])
+    if current_user.room_ids.index(@room.id) == nil
+       redirect_to root_path
+    
+    else current_user.room_ids[current_user.room_ids.index(@room.id)] == @room.id
+    @message = Message.new
     @messages = @room.messages.includes(:user)
 
     user = @room.room_users.pluck(:user_id)
     user.delete(current_user.id)
     @user = User.find(user[0])
+    end
   end
 
   def create
+
     @room = Room.find(params[:room_id])
+    user = @room.room_users.pluck(:user_id)
+    user.delete(current_user.id)
+    @user = User.find(user[0])
     @message = @room.messages.new(message_params)
     if @message.save
       redirect_to room_messages_path(@room,anchor: 'u')
     else
       @message = @room.messages.includes(:user)
-      render :index
-    end  
+      redirect_to room_messages_path(@room,anchor: 'u')
+    end
   end
 
   def destroy
